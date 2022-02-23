aed4e8797b3de1e8f10f91c12d6b5530292dfc10
anresz73 <andresreisz@gmail.com>
2022-02-22 22:45:19 -0300

    white_noise function add

--- white_noise/white_noise.py
+++ white_noise/white_noise.py
@@ -11,25 +11,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-# Create numpy random matrix with photo pixel data 
-h, au = 400, (1. + 5. ** .5) / 2.
-white_noise_image = np.random.randint(low = 10, high = 245, size = (h, int(h * au), 3, 10))
+def white_noise(
+  h = 200,
+  iframes = 10,
+  animation_name = 'animation'):
+  # Create numpy random matrix with photo pixel data 
+  au = (1. + 5. ** .5) / 2.
+  white_noise_image = np.random.randint(low = 10, high = 245, size = (h, int(h * au), 3, iframes))
 
-# Initialize Figure
-fig, ax = plt.subplots()
-ax.axis('off')
+  # Initialize Figure
+  fig, ax = plt.subplots()
+  plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
+  ax.axis('off')
+  ax.margins(0.)
+  #ax.spines[['right', 'left']].set_visible(False)
 
-# Frame Gen Function
-def func(frame):
-  return ax.imshow(white_noise_image[:, :, :, frame])
+  # Frame Gen Function
+  def func(frame):
+    return ax.imshow(white_noise_image[:, :, :, frame])
 
-#FuncAnimation from matplotlib
-anim = FuncAnimation(
-    fig = fig,
-    func = func,
-    frames = 10,
-    cache_frame_data = False
-)
+  #FuncAnimation from matplotlib
+  anim = FuncAnimation(
+      fig = fig,
+      func = func,
+      frames = iframes,
+      cache_frame_data = False
+  )
 
-#Save gif
-anim.save('./animation.gif', writer='pillow', fps=60)
+  #Save gif
+  anim.save(
+    f'./{animation_name}.gif',
+    writer='pillow',
+    fps=60,
+    #bbox_inches='tight',
+    #savefig_kwargs = {'pad_inches' : 0.,
+    #                  'bbox_inches' : 'tight'}
+    )
