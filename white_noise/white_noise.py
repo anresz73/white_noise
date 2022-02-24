3d207e19a329b1bd94dabab19916716f4ff0ba9a
anresz73 <andresreisz@gmail.com>
2022-02-24 01:14:01 -0300

    white_noise.py blank spaces removed

--- white_noise/white_noise.py
+++ white_noise/white_noise.py
@@ -9,22 +9,32 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
+from matplotlib.animation import FuncAnimation, ArtistAnimation
 
 def white_noise(
   h = 200,
   iframes = 10,
   animation_name = 'animation'):
   # Create numpy random matrix with photo pixel data
-  au = (1. + 5. ** .5) / 2.
+  #au = (1. + 5. ** .5) / 2.
+  au = 1.6
   white_noise_image = np.random.randint(low = 10, high = 245, size = (h, int(h * au), 3, iframes))
 
   # Initialize Figure
-  fig, ax = plt.subplots()
-  plt.subplots_adjust(left=0, right=1, top=1, bottom=0, wspace=0, hspace=0)
+  fig, ax = plt.subplots(figsize = (16, 10))
+  #fig.patch.set_alpha(0.)
+  #fig.tight_layout()
+  fig.subplots_adjust(left=0, right=1, top=1, bottom=0, wspace=0, hspace=0)
   ax.axis('off')
-  ax.margins(0.)
+  #ax.set_ylim(-70, 200)
+  #ax.margins(0., 0., tight = True)
   #ax.spines[['right', 'left']].set_visible(False)
+
+
+  # turn off axis spines
+  #ax.xaxis.set_visible(False)
+  #ax.yaxis.set_visible(False)
+  #ax.set_frame_on(False)
 
   # Frame Gen Function
   def func(frame):
@@ -37,6 +47,13 @@ def white_noise(
       frames = iframes,
       cache_frame_data = False
   )
+
+  #ArtistAnimation
+  #artists = [[ax.imshow(white_noise_image[:, :, :, e], animated = True) for e in range(iframes)]]
+  #anim = ArtistAnimation(
+  #  fig = fig,
+  #  artists = artists
+  #)
 
   #Save gif
   anim.save(
